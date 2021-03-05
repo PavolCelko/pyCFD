95827f8b50abc59dc82e6220852eec1b114f7c05
Pavol Celko <palko@celko>
2021-03-05 20:40:41 +0100

    non periodic BCs. based on cavity flow

--- example.py
+++ example.py
@@ -3,65 +3,42 @@ from matplotlib import pyplot, cm
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def build_up_b(rho, dt, dx, dy, u, v):
-	b = numpy.zeros_like(u)
-	b[1:-1, 1:-1] = (rho * (1 / dt * ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx) +
-									  (v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy)) -
-							((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx)) ** 2 -
+def build_up_b(b, rho, dt, u, v, dx, dy):
+	b[1:-1, 1:-1] = (rho * ( - ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx)) ** 2 -
 							2 * ((u[2:, 1:-1] - u[0:-2, 1:-1]) / (2 * dy) *
 								 (v[1:-1, 2:] - v[1:-1, 0:-2]) / (2 * dx)) -
 							((v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy)) ** 2))
 
-	# Periodic BC Pressure @ x = 2
-	b[1:-1, -1] = (rho * (1 / dt * ((u[1:-1, 0] - u[1:-1, -2]) / (2 * dx) +
-									(v[2:, -1] - v[0:-2, -1]) / (2 * dy)) -
-						  ((u[1:-1, 0] - u[1:-1, -2]) / (2 * dx)) ** 2 -
-						  2 * ((u[2:, -1] - u[0:-2, -1]) / (2 * dy) *
-							   (v[1:-1, 0] - v[1:-1, -2]) / (2 * dx)) -
-						  ((v[2:, -1] - v[0:-2, -1]) / (2 * dy)) ** 2))
-
-	# Periodic BC Pressure @ x = 0
-	b[1:-1, 0] = (rho * (1 / dt * ((u[1:-1, 1] - u[1:-1, -1]) / (2 * dx) +
-								   (v[2:, 0] - v[0:-2, 0]) / (2 * dy)) -
-						 ((u[1:-1, 1] - u[1:-1, -1]) / (2 * dx)) ** 2 -
-						 2 * ((u[2:, 0] - u[0:-2, 0]) / (2 * dy) *
-							  (v[1:-1, 1] - v[1:-1, -1]) / (2 * dx)) -
-						 ((v[2:, 0] - v[0:-2, 0]) / (2 * dy)) ** 2))
-
 	return b
 
 
-def pressure_poisson_periodic(p, dx, dy):
+def pressure_poisson(p, dx, dy, b):
 	pn = numpy.empty_like(p)
+	pn = p.copy()
 
 	for q in range(nit):
 		pn = p.copy()
 		p[1:-1, 1:-1] = (((pn[1:-1, 2:] + pn[1:-1, 0:-2]) * dy ** 2 +
 						  (pn[2:, 1:-1] + pn[0:-2, 1:-1]) * dx ** 2) /
 						 (2 * (dx ** 2 + dy ** 2)) -
-						 dx ** 2 * dy ** 2 / (2 * (dx ** 2 + dy ** 2)) * b[1:-1, 1:-1])
+						 dx ** 2 * dy ** 2 / (2 * (dx ** 2 + dy ** 2)) *
+						 b[1:-1, 1:-1])
 
-		# Periodic BC Pressure @ x = 2
-		p[1:-1, -1] = (((pn[1:-1, 0] + pn[1:-1, -2]) * dy ** 2 +
-						(pn[2:, -1] + pn[0:-2, -1]) * dx ** 2) /
-					   (2 * (dx ** 2 + dy ** 2)) -
-					   dx ** 2 * dy ** 2 / (2 * (dx ** 2 + dy ** 2)) * b[1:-1, -1])
+		# p[:, -1] = p[:, -2]  # dp/dx = 0 at x = 2
+		# p[0, :] = p[1, :]  # dp/dy = 0 at y = 0
+		# p[:, 0] = p[:, 1]  # dp/dx = 0 at x = 0
+		# p[-1, :] = 0  # p = 0 at y = 2
 
-		# Periodic BC Pressure @ x = 0
-		p[1:-1, 0] = (((pn[1:-1, 1] + pn[1:-1, -1]) * dy ** 2 +
-					   (pn[2:, 0] + pn[0:-2, 0]) * dx ** 2) /
-					  (2 * (dx ** 2 + dy ** 2)) -
-					  dx ** 2 * dy ** 2 / (2 * (dx ** 2 + dy ** 2)) * b[1:-1, 0])
-
-		# Wall boundary conditions, pressure
-		p[-1, :] = p[-2, :]  # dp/dy = 0 at y = 2
+		p[:, -1] = 0  # p = 0 at x = 2
+		p[:, 0] = 100   # p = MAX at x = 0
 		p[0, :] = p[1, :]  # dp/dy = 0 at y = 0
+		p[-1, :] = p[-2, :]  # dp/dy = 0 at y = 2
 
 	return p
 
 
 ##variable declarations
-pipe_len   = 20
+pipe_len   = 10
 pipe_width = 2
 
 nx = pipe_len * 20 + 1
@@ -94,98 +71,69 @@ pn = numpy.ones((ny, nx))
 
 b = numpy.zeros((ny, nx))
 
-udiff = 1
-stepcount = 0
 
-while udiff > .001:
-	un = u.copy()
-	vn = v.copy()
+def cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu):
+	un = numpy.empty_like(u)
+	vn = numpy.empty_like(v)
+	b = numpy.zeros((ny, nx))
 
-	b = build_up_b(rho, dt, dx, dy, u, v)
-	p = pressure_poisson_periodic(p, dx, dy)
+	for n in range(nt):
+		un = u.copy()
+		vn = v.copy()
 
-	u[1:-1, 1:-1] = (un[1:-1, 1:-1] -
-					un[1:-1, 1:-1] * dt / dx *
-					(un[1:-1, 1:-1] - un[1:-1, 0:-2]) -
-					vn[1:-1, 1:-1] * dt / dy *
-					(un[1:-1, 1:-1] - un[0:-2, 1:-1]) -
-					dt / (2 * rho * dx) *
-					(p[1:-1, 2:] - p[1:-1, 0:-2]) +
-					nu * (dt / dx ** 2 *
-						(un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, 0:-2]) +
-						dt / dy ** 2 *
-						(un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1])) +
-					F * dt)
+		b = build_up_b(b, rho, dt, u, v, dx, dy)
+		p = pressure_poisson(p, dx, dy, b)
 
-	v[1:-1, 1:-1] = (vn[1:-1, 1:-1] -
-					un[1:-1, 1:-1] * dt / dx *
-					(vn[1:-1, 1:-1] - vn[1:-1, 0:-2]) -
-					vn[1:-1, 1:-1] * dt / dy *
-					(vn[1:-1, 1:-1] - vn[0:-2, 1:-1]) -
-					dt / (2 * rho * dy) *
-					(p[2:, 1:-1] - p[0:-2, 1:-1]) +
-					nu * (dt / dx ** 2 *
-						(vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, 0:-2]) +
-						dt / dy ** 2 *
-						(vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1])))
+		u[1:-1, 1:-1] = (un[1:-1, 1:-1] -
+						 un[1:-1, 1:-1] * dt / dx *
+						 (un[1:-1, 1:-1] - un[1:-1, 0:-2]) -
+						 vn[1:-1, 1:-1] * dt / dy *
+						 (un[1:-1, 1:-1] - un[0:-2, 1:-1]) -
+						 dt / (2 * rho * dx) * (p[1:-1, 2:] - p[1:-1, 0:-2]) +
+						 nu * (dt / dx ** 2 *
+							   (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, 0:-2]) +
+							   dt / dy ** 2 *
+							   (un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1])))
 
-	# Periodic BC u @ x = 2
-	u[1:-1, -1] = (un[1:-1, -1] - un[1:-1, -1] * dt / dx *
-					(un[1:-1, -1] - un[1:-1, -2]) -
-					vn[1:-1, -1] * dt / dy *
-					(un[1:-1, -1] - un[0:-2, -1]) -
-					dt / (2 * rho * dx) *
-					(p[1:-1, 0] - p[1:-1, -2]) +
-					nu * (dt / dx ** 2 *
-						(un[1:-1, 0] - 2 * un[1:-1, -1] + un[1:-1, -2]) +
-						dt / dy ** 2 *
-						(un[2:, -1] - 2 * un[1:-1, -1] + un[0:-2, -1])) + F * dt)
+		v[1:-1, 1:-1] = (vn[1:-1, 1:-1] -
+						 un[1:-1, 1:-1] * dt / dx *
+						 (vn[1:-1, 1:-1] - vn[1:-1, 0:-2]) -
+						 vn[1:-1, 1:-1] * dt / dy *
+						 (vn[1:-1, 1:-1] - vn[0:-2, 1:-1]) -
+						 dt / (2 * rho * dy) * (p[2:, 1:-1] - p[0:-2, 1:-1]) +
+						 nu * (dt / dx ** 2 *
+							   (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, 0:-2]) +
+							   dt / dy ** 2 *
+							   (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1])))
 
-	# Periodic BC u @ x = 0
-	u[1:-1, 0] = (un[1:-1, 0] - un[1:-1, 0] * dt / dx *
-					(un[1:-1, 0] - un[1:-1, -1]) -
-					vn[1:-1, 0] * dt / dy *
-					(un[1:-1, 0] - un[0:-2, 0]) -
-					dt / (2 * rho * dx) *
-					(p[1:-1, 1] - p[1:-1, -1]) +
-					nu * (dt / dx ** 2 *
-						(un[1:-1, 1] - 2 * un[1:-1, 0] + un[1:-1, -1]) +
-						dt / dy ** 2 *
-						(un[2:, 0] - 2 * un[1:-1, 0] + un[0:-2, 0])) + F * dt)
+		# u[0, :] = 0
+		# u[:, 0] = 0
+		# u[:, -1] = 0
+		# u[-1, :] = 1  # set velocity on cavity lid equal to 1
+		# v[0, :] = 0
+		# v[-1, :] = 0
+		# v[:, 0] = 0
+		# v[:, -1] = 0
 
-	# Periodic BC v @ x = 2
-	v[1:-1, -1] = (vn[1:-1, -1] - un[1:-1, -1] * dt / dx *
-					(vn[1:-1, -1] - vn[1:-1, -2]) -
-					vn[1:-1, -1] * dt / dy *
-					(vn[1:-1, -1] - vn[0:-2, -1]) -
-					dt / (2 * rho * dy) *
-					(p[2:, -1] - p[0:-2, -1]) +
-					nu * (dt / dx ** 2 *
-						(vn[1:-1, 0] - 2 * vn[1:-1, -1] + vn[1:-1, -2]) +
-						dt / dy ** 2 *
-						(vn[2:, -1] - 2 * vn[1:-1, -1] + vn[0:-2, -1])))
+		u[0, :] = 0
+		v[0, :] = 0
+		u[-1, :] = 0
+		v[-1, :] = 0
 
-	# Periodic BC v @ x = 0
-	v[1:-1, 0] = (vn[1:-1, 0] - un[1:-1, 0] * dt / dx *
-					(vn[1:-1, 0] - vn[1:-1, -1]) -
-					vn[1:-1, 0] * dt / dy *
-					(vn[1:-1, 0] - vn[0:-2, 0]) -
-					dt / (2 * rho * dy) *
-					(p[2:, 0] - p[0:-2, 0]) +
-					nu * (dt / dx ** 2 *
-						(vn[1:-1, 1] - 2 * vn[1:-1, 0] + vn[1:-1, -1]) +
-						dt / dy ** 2 *
-						(vn[2:, 0] - 2 * vn[1:-1, 0] + vn[0:-2, 0])))
+		u[:, 0] = u[:, 1]
+		u[:, -1] = u[:, -2]
+		v[:, 0] = v[:, 1]
+		v[:, -1] = v[:, -2]
 
-	# Wall BC: u,v = 0 @ y = 0,2
-	u[0, :] = 0
-	u[-1, :] = 0
-	v[0, :] = 0
-	v[-1, :] = 0
+	return u, v, p
 
-	udiff = (numpy.sum(u) - numpy.sum(un)) / numpy.sum(u)
-	stepcount += 1
-	print(stepcount)
+
+u = numpy.zeros((ny, nx))
+v = numpy.zeros((ny, nx))
+p = numpy.zeros((ny, nx))
+b = numpy.zeros((ny, nx))
+nt = 100
+u, v, p = cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu)
 
 pyplot.imshow(u, cmap='jet')
 pyplot.colorbar()
