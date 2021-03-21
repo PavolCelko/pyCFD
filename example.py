0a259970c76457706b511c8dd380b5270755d2b8
Pavol Celko <palko@celko>
2021-03-21 14:40:25 +0100

    API and code clean up script run for 44 seconds nu=0.0 durat=1ms		dt=0.100us nx=101 ny=15 pressure inlet  : 1500000.00 mean velocity inlet   : 112.39 mean velocity outlet  : 112.39 outlet flow : 0.731 cm2/ms

--- example.py
+++ example.py
@@ -1,11 +1,9 @@
 import numpy
-from matplotlib import pyplot, cm
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot
 import time
 
-start_time = time.time()
 
-def build_up_b(b, rho, dt, u, v, dx, dy):
+def build_up_b(xx, yy, b, rho, u, v):
 	b[1:-1, 1:-1] = (rho * ( - ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (xx[1:-1, 2:] - xx[1:-1, 0:-2])) ** 2 -
 							2 * ((u[2:, 1:-1] - u[0:-2, 1:-1]) / (yy[2:, 1:-1] - yy[0:-2, 1:-1]) *
 								 (v[1:-1, 2:] - v[1:-1, 0:-2]) / (xx[1:-1, 2:] - xx[1:-1, 0:-2])) -
@@ -14,13 +12,11 @@ def build_up_b(b, rho, dt, u, v, dx, dy):
 	return b
 
 
-def pressure_poisson(p, dx, dy, b):
-	pn = numpy.empty_like(p)
-	pn = p.copy()
+def pressure_poisson(xx, yy, p, b):
 	dx = xx[1:-1, 1:-1] - xx[1:-1, 0:-2]
 	dy = yy[1:-1, 1:-1] - yy[0:-2, 1:-1]
 
-	for q in range(nit):
+	for q in range(npit):
 		pn = p.copy()
 		p[1:-1, 1:-1] = (((pn[1:-1, 2:] + pn[1:-1, 0:-2]) * dy ** 2 +
 						  (pn[2:, 1:-1] + pn[0:-2, 1:-1]) * dx ** 2) /
@@ -42,55 +38,15 @@ def pressure_poisson(p, dx, dy, b):
 	return p
 
 
-##variable declarations
-pipe_len   =  5 * 1e-3
-pipe_width =  0.7 * 1e-3
-
-nx = int(pipe_len * 20 * 1e3) + 1
-ny = int(pipe_width * 20 * 1e3) + 1
-# nt = 10
-nit = 50
-
-dx = pipe_len / (nx - 1)
-dy = pipe_width / (ny - 1)
-x = numpy.linspace(0, pipe_len, nx)
-y = numpy.linspace(0, pipe_width, ny)
-X, Y = numpy.meshgrid(x, y)
-# pipe_array_x = numpy.array([x for i in range(len(y))])
-# pipe_array_y = numpy.array([y for i in range(len(x))])
-xx = numpy.array([x for i in range(len(y))])
-yy = numpy.array([y for i in range(len(x))]).transpose()
-##physical variables
-rho = 868
-nu = 50e-6
-dt = .001/10000
-durat = 0.001
-p_inlet = 15e5
-
-#initial conditions
-u = numpy.zeros((ny, nx))
-un = numpy.zeros((ny, nx))
-
-v = numpy.zeros((ny, nx))
-vn = numpy.zeros((ny, nx))
-
-p = numpy.ones((ny, nx))
-pn = numpy.ones((ny, nx))
-
-b = numpy.zeros((ny, nx))
-
-
-def cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu):
-	un = numpy.empty_like(u)
-	vn = numpy.empty_like(v)
-	b = numpy.zeros((ny, nx))
+def cavity_flow(nt, dt, xx, yy, u, v, p, rho, nu):
+	b_field = numpy.zeros((ny, nx))
 
 	for n in range(nt):
 		un = u.copy()
 		vn = v.copy()
 
-		b = build_up_b(b, rho, dt, u, v, dx, dy)
-		p = pressure_poisson(p, dx, dy, b)
+		b_field = build_up_b(xx, yy, b_field, rho, u, v)
+		p = pressure_poisson(xx, yy, p, b_field)
 
 		dx = xx[1:-1, 1:-1] - xx[1:-1, 0:-2]
 		dy = yy[1:-1, 1:-1] - yy[0:-2, 1:-1]
@@ -134,12 +90,36 @@ def cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu):
 	return u, v, p
 
 
+# geometry and scaling
+pipe_len   = 5 * 1e-3
+pipe_width = 0.7 * 1e-3
+nx = int(pipe_len * 20 * 1e3) + 1
+ny = int(pipe_width * 20 * 1e3) + 1
+x = numpy.linspace(0, pipe_len, nx)
+y = numpy.linspace(0, pipe_width, ny)
+X, Y = numpy.meshgrid(x, y)
+xx = numpy.array([x for i in range(len(y))])
+yy = numpy.array([y for i in range(len(x))]).transpose()
+
+# fluid physics constants
+rho = 868
+nu = 50e-6
+
+# timings
+dt = .001/10000
+durat = 0.001
+nt = int(durat / dt)
+start_time = time.time()
+
+# pressure iterator
+npit = 50
+p_inlet = 15e5
+
 u = numpy.zeros((ny, nx))
 v = numpy.zeros((ny, nx))
 p = numpy.zeros((ny, nx))
-b = numpy.zeros((ny, nx))
-nt = int(durat / dt)
-u, v, p = cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu)
+
+u, v, p = cavity_flow(nt, dt, xx, yy, u, v, p, rho, nu)
 
 sum_u = numpy.zeros((1, nx))
 for i in range(nx):
@@ -152,7 +132,7 @@ print("nu={:0.1f} durat={:0.0f}ms		dt={:0.3f}us nx={:d} ny={:d}".format(nu, dura
 print("pressure inlet  : {:0.2f}".format(*tuple(p[int(ny/2), :1])))
 print("mean velocity inlet   : {:0.2f} ".format((numpy.mean(u[1:-1, 0]))))
 print("mean velocity outlet  : {:0.2f} ".format((numpy.mean(u[1:-1, -1]))))
-print("outlet flow : {:0.3f} cm2/ms".format(sum(u[:, -1] * dy) * 1e4 / 1e3))
+print("outlet flow : {:0.3f} cm2/ms".format(sum(u[1:-1, -1] * (yy[1:-1, -1] - yy[0:-2, -1])) * 1e4 / 1e3))
 
 numpy.save("x_coord_array", x)
 numpy.save("y_coord_array", y)
@@ -177,11 +157,3 @@ pyplot.subplot(4, 1, 4)
 pyplot.plot(x, sum_u[0, :])
 
 pyplot.show()
-
-# pyplot.imshow(u, cmap='jet')
-# pyplot.imshow(p, cmap='jet')
-# pyplot.colorbar()
-# pyplot.show()
-
-# pyplot.plot(x, u[20, :])
-# pyplot.show()
